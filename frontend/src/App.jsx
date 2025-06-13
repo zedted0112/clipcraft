2efc29a0ec406fc51df36ac6ed1c753cc42c0600
HimalayanCoder <nitinranaofficial456l@gmail.com>
2025-06-12 23:02:14 -0700

    added frontend folder with tailwindcss+vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react'
+function App() {
+  return (
+    <div className="text-4xl text-purple-600 font-bold">
+      Hello Tailwind! 😄
+    </div>
+  )
+}
+
+export default App
